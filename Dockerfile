14b01b67835bc877aabce2c9aba6490e39839637
raymondmouthaan <raymondmmouthaan@gmail.com>
2018-04-16 21:19:55 +0200

    working version with docker on mac

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM arm32v7/openjdk:8-jdk-slim-stretch
+FROM openjdk:8-jdk-alpine
 VOLUME /tmp
 ARG JAR_FILE
 ADD ${JAR_FILE} app.jar
--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -5,16 +5,25 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 @SpringBootApplication
 @RestController
 public class Application {
 
     @RequestMapping("/")
     public String home() {
-        return "Hello Docker World";
+        LocalDate localDate = LocalDate.now();
+        LocalTime localTime = LocalTime.now();
+
+        return "Hello Docker World! Today is " + localDate + " and the time is " + localTime;
     }
 
     public static void main(String[] args) {
+
+
         SpringApplication.run(Application.class, args);
     }
 }
